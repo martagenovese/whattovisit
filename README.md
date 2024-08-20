614a4df5d7cc2748ea862d7898d954f5c2007218
martagenovese <19841@studenti.marconiverona.edu.it>
2024-08-20 19:07:39 +0200

    registration site and basics of index. connection to db for registration

---
+++ README.md
@@ -0,0 +1,5 @@
+# whattovisit
+## map with places to visit
+
+webapp that displays a map (potentially sharable) with places to visit (potentially add places to stay and to eat).
+
---
+++ cred.php
@@ -0,0 +1,7 @@
+$conn = new mysqli('localhost', 'bot', 'Password.123', 'whattovisit');
+
+$host = 'localhost';
+$username = 'bot';
+$password = 'Password.123';
+$db = 'whattovisit';
+
