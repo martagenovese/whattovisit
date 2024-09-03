30623e7d5d428118925d232b01db066e55990008
martagenovese <19841@studenti.marconiverona.edu.it>
2024-09-03 21:15:00 +0200

    mandatory login

--- backend/script_login.js
+++ backend/script_login.js
@@ -1,23 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const loginForm = document.getElementById('loginForm');
     const registerForm = document.getElementById('registerForm');
 
     if (loginForm) {
-        loginForm.addEventListener('submit', function(event) {
-            handleFormSubmit(event, 'loginForm');
-        });
+        loginForm.addEventListener('submit', (event) => handleFormSubmit(event, 'loginForm'));
+    }
+    if (registerForm) {
+        registerForm.addEventListener('submit', (event) => handleFormSubmit(event, 'registerForm'));
     }
 
-    if (registerForm) {
-        registerForm.addEventListener('submit', function(event) {
-            handleFormSubmit(event, 'registerForm');
-        });
-    }
+    checkLoginStatus();
 });
 
 async function handleFormSubmit(event, formId) {
     event.preventDefault();
-
     const formData = new FormData(event.target);
 
     try {
@@ -31,18 +27,27 @@ async function handleFormSubmit(event, formId) {
         try {
             const result = JSON.parse(resultText);
             if (result.status === 'success') {
-                window.location.href = 'frontend/start.html'; // Redirect on success
+                localStorage.setItem('isLoggedIn', 'true');
+                window.location.href = 'frontend/start.html';
             } else {
-                displayMessage(result, formId + 'Message');
+                displayMessage(result, formId);
             }
         } catch (jsonError) {
             console.error('Error parsing JSON:', jsonError);
             console.error('Raw Response:', resultText);
-            displayMessage({ status: 'error', message: 'Invalid response from server.' }, formId + 'Message');
+            displayMessage({ status: 'error', message: 'Invalid response from server.' }, formId);
         }
     } catch (error) {
         console.error('Error:', error);
-        displayMessage({ status: 'error', message: 'An unexpected error occurred.' }, formId + 'Message');
+        displayMessage({ status: 'error', message: 'An unexpected error occurred.' }, formId);
+    }
+}
+
+function checkLoginStatus() {
+    const isLoggedIn = localStorage.getItem('isLoggedIn');
+
+    if (!isLoggedIn) {
+        window.location.href = 'frontend/login.html';
     }
 }
 
